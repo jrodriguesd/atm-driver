994150db9e3a9b874dc397865511b401af39d321
Jose Rodrigues <jrodriguesd@gmail.com>
2022-05-15 17:08:13 -0500

    Update comments at the top of a sourcecode files

--- modules/atm-driver-web/src/main/java/org/jfrd/webapp/GenericDivView.java
+++ modules/atm-driver-web/src/main/java/org/jfrd/webapp/GenericDivView.java
@@ -1,21 +1,24 @@
 /*
- * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2021 jPOS Software SRL
+ * This file is part of atm-driver.
+ * Copyright (C) 2021-2022
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * atm-driver is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * atm-driver is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with atm-driver. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @author <a href="mailto:j@rodriguesd.org">Jose Rodrigues D.</a>
+ */
 package org.jfrd.webapp;
 
 import com.vaadin.navigator.View;
